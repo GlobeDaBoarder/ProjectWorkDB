fc079a2719b0dba295727ced8103d02d2d1f91b6
Gleb Ivashyn <gleb.ivashyn@stud.vgtu.lt>
2023-01-18 03:13:45 +0100

    functionality to create DB with CLI

--- src/main/java/base/CLI/LocalDbCommand.java
+++ src/main/java/base/CLI/LocalDbCommand.java
@@ -3,6 +3,7 @@ package base.CLI;
 import base.CLI.Subcommands.CreateDbCommand;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 @Command(
         name = "db",
@@ -16,8 +17,12 @@ public class LocalDbCommand implements Runnable{
     public static void main(String[] args) {
         CommandLine.run(new LocalDbCommand(), args);
     }
+
+    @Option(names = {"-v", "--verbose"})
+    boolean verbose;
     @Override
     public void run() {
-        System.out.println("test");
+        if(verbose)
+            System.out.println("test");
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,18 @@
-//package org.example;
-//
-//import base.database.*;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        DatabaseFactory factory = new FileDatabaseFactory();
-//        Database db = factory.createDatabase("db");
-//        EntryList collection1 = db.createCollection("col1");
-//        EntryList collection2 = db.createCollection("col2");
-////        collection1.add(new Entry("{ \"name\": \"Baeldung\", \"java\": \"true\" }"));
-////        collection1.add(new Entry("{ \"name\": \"Globe\", \"java\": \"true\" }"));
-//        EntryList collection3 = db.createCollection("col1");
-//        Database db2 = factory.createDatabase("db");
-//
-//        System.out.println("done");
-//    }
-//}
+package org.example;
+
+import base.database.*;
+
+public class Main {
+    public static void main(String[] args) {
+        DatabaseFactory factory = new FileDatabaseFactory();
+        Database db = factory.createDatabase("db");
+        EntryList collection1 = db.createCollection("col1");
+        EntryList collection2 = db.createCollection("col2");
+//        collection1.add(new Entry("{ \"name\": \"Baeldung\", \"java\": \"true\" }"));
+//        collection1.add(new Entry("{ \"name\": \"Globe\", \"java\": \"true\" }"));
+        EntryList collection3 = db.createCollection("col1");
+        Database db2 = factory.createDatabase("db");
+
+        System.out.println("done");
+    }
+}
